9d500239595bf06f7114ee21646fadc3653b7255
Snavyy <1337Snavy@gmail.com>
2022-09-19 06:43:56 +0200

    added constructor methods to Command

--- src/main/java/net/heartbyte/lerpc/proto/Command.java
+++ src/main/java/net/heartbyte/lerpc/proto/Command.java
@@ -2,6 +2,7 @@ package net.heartbyte.lerpc.proto;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Command {
@@ -10,4 +11,21 @@ public class Command {
     @SerializedName("ns") public String              namespace;
     @SerializedName("mt") public String              method;
     @SerializedName("pm") public Map<String, Object> params;
+
+    public Command setNamespace(String value) {
+        this.namespace = value;
+        return this;
+    }
+
+    public Command setMethod(String value) {
+        this.method = value;
+        return this;
+    }
+
+    public Command setParam(String key, Object value) {
+        if (this.params == null)
+            this.params = new HashMap<>();
+        this.params.put(key, value);
+        return this;
+    }
 }
